a34b3d10f506516267d2de2de593383ea642339b
Jodee Harris <jodee_harris@yahoo.com>
2023-10-25 20:57:24 -0500

    Update geoJson.js

--- java/geoJson.js
+++ java/geoJson.js
@@ -1,4 +1,4 @@
-function createMap(gunViolence) {
+function createMap(_gunViolence) {
  // Create the tile layer that will be the background of our map.
     let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
